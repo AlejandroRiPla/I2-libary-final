347d0743ef53e0cc123dd207f346316846bd32b3
JESUS ALEJANDRO RIVERA PLATA <89490791+AlejandroRiPla@users.noreply.github.com>
2021-09-01 22:01:11 -0500

    Biblioteca lista para publicación

--- lib/src/convertirACentigrados.c
+++
@@ -1,9 +0,0 @@
-#include "../include/fisica.h"
-/** @brief Convierte grados Farenghet a Centigrados
- * @param f double Grados Farenheit
- * @returns c double Grados Centigrados
- */
-double convertirACentigrados(double f)
-{
-	return ((f-32)/1.8);
-}
